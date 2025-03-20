60b3cb97861412f495823d072d2ae7dc3f343908
Selim Eser <2211482@stud.hs-mannheim.de>
2025-03-20 16:20:32 +0100

    First Home styles

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,15 +1,22 @@
 import { Link } from "react-router-dom";
+import css from "./styles.module.css";
+import Logo from "../../assets/logo.png";
 
 export default function Home() {
   return (
-    <>
-      <h1>Home</h1>
-      <br />
-      <Link to="/facts">Facts</Link>
-      <br />
-      <Link to="/kippp">Kippp</Link>
-      <br />
-      <Link to="/challenge">Challenge</Link>
-    </>
+    <div className={css.home}>
+      <img src={Logo} className={css.logo} />
+      <div className={css.buttonContainer}>
+        <div className={css.button}>
+          <Link to="/facts">Facts</Link>
+        </div>
+        <div className={css.button} id={css.middleButton}>
+          <Link to="/kippp">Kippp</Link>
+        </div>
+        <div className={css.button}>
+          <Link to="/challenge">Challenge</Link>
+        </div>
+      </div>
+    </div>
   );
 }
