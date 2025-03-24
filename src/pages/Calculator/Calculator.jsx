2db43cac6e3d2a6e98b5a36b159fa15f2ef37efa
selim <selimyag.eser@gmail.com>
2025-03-24 22:25:31 +0100

    Finished Calculator logic

--- src/pages/Calculator/Calculator.jsx
+++ src/pages/Calculator/Calculator.jsx
@@ -1,28 +1,88 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import css from "./styles.module.css";
 
 export default function Calculator() {
-  const [result, setResult] = useState("");
+  //Alle lokalen Zustandsvariablen
+  const [result1, setResult1] = useState(0);
+  const [result2, setResult2] = useState(0);
+  const [result3, setResult3] = useState(0);
+  const [result4, setResult4] = useState(0);
+  const [result5, setResult5] = useState(0);
+  const [resultAll, setResultAll] = useState(0);
   const [pageNumber, setPageNumber] = useState(0);
   const [numPeople, setNumPeople] = useState(1);
   const [livingArea, setLivingArea] = useState(95);
   const [heatingType, setHeatingType] = useState(false);
   const [electricType, setElectricype] = useState(false);
-  const [hasNoCar, setHasNoCar] = useState(false);
+  const [hasNoCar, setHasNoCar] = useState(true);
   const [useNoPublicTransport, setUseNoPublicTransport] = useState(false);
-  const [airTravelEurope, setAirTravelEurope] = useState(3);
-  const [airTravelTranscontinental, setAirTravelTranscontinental] = useState(0);
+  const [airTravelEurope, setAirTravelEurope] = useState(1);
+  const [airTravelTranscontinental, setAirTravelTranscontinental] = useState(3);
   const [eatNoMeat, setEatNoMeat] = useState(false);
   const [eatNoMilk, setEatNoMilk] = useState(false);
-  const [income, setIncome] = useState(3000);
+  const [income, setIncome] = useState(1500);
 
-  function calculateResult() {
-    return "123";
+  //Wenn das Ergebnis feststeht kann die Klimaneutral Differenz berechnet werden
+  useEffect(() => {
+    calculateResult5();
+  }, [resultAll]);
+
+  //Alle Formeln/Rechnungsmethoden für den CO2 Rechner
+  //mit Aktualisierung von zugehörigen Zustandsvariablen
+
+  function calculateResult1() {
+    const H = heatingType ? 0.01 : 0.0504;
+    const S = electricType ? 0.1 : 0.41;
+    const result = livingArea * H + numPeople * S;
+    setResult1(Math.round(result * 100) / 100);
+    return Math.round(result * 100) / 100;
+  }
+
+  function calculateResult2() {
+    const A = hasNoCar ? 0 : 1;
+    const Ö = useNoPublicTransport ? 0 : 1;
+    const result =
+      A * 1.6 +
+      Ö * -0.6 +
+      airTravelEurope * 0.21 +
+      airTravelTranscontinental * 0.38;
+    setResult2(Math.round(result * 100) / 100);
+    return Math.round(result * 100) / 100;
+  }
+
+  function calculateResult3() {
+    const F = eatNoMeat ? 0 : 1.21;
+    const M = eatNoMilk ? 0 : 0.31;
+    const result = F + M;
+    setResult3(Math.round(result * 100) / 100);
+    return Math.round(result * 100) / 100;
+  }
+
+  function calculateResult4() {
+    const result = income * 0.000954;
+    setResult4(Math.round(result * 100) / 100);
+    return Math.round(result * 100) / 100;
+  }
+
+  function calculateResult5() {
+    const result = resultAll - 2.5;
+    setResult5(Math.round(result * 100) / 100);
+    return Math.round(result * 100) / 100;
+  }
+
+  function calculateResultAll() {
+    const result =
+      calculateResult1() +
+      calculateResult2() +
+      calculateResult3() +
+      calculateResult4() +
+      1.16;
+    setResultAll(Math.round(result * 100) / 100);
   }
 
   return (
     <div className={css.calculator}>
-      <div className={css.heading}>{"CO₂ Rechner"}</div>
+      <div className={css.heading}>{"CO₂e Rechner"}</div>
       <div className={css.card}>
         {pageNumber == 0 ? (
           <div className={css.page}>
@@ -238,24 +298,36 @@ export default function Calculator() {
             </div>
           </div>
         ) : pageNumber == 4 ? (
-          <div className={css.page}>
+          <div className={css.page} style={{ rowGap: "1.2em" }}>
             <div className={css.subHeading}>Ergebnis</div>
             <div className={css.text}>Haus/Wohnung</div>
-            <div className={css.text2}>{result}</div>
+            <div className={css.text2}>{result1 + " t"}</div>
             <div className={css.text}>Mobilität</div>
-            <div className={css.text2}>{result}</div>
+            <div className={css.text2}>{result2 + " t"}</div>
             <div className={css.text}>Ernährung</div>
-            <div className={css.text2}>{result}</div>
+            <div className={css.text2}>{result3 + " t"}</div>
             <div className={css.text}>
-              Sonstiger Konsum/Öffentliche Emmisionen
+              Sonstiger Konsum | Öffentliche Emmisionen
             </div>
-            <div className={css.text2}>{result}</div>
+            <div className={css.text2}>{result4 + " | 1,16" + " t"}</div>
             <div className={css.text} id={css.result}>
-              Gesamt/Deutscher Durchschnitt
+              Gesamt
             </div>
-            <div className={css.text2} id={css.result}>
-              {result + "/10,41" + " t CO₂/Jahr"}
+            <div
+              className={css.text2}
+              id={css.result}
+              style={{ textDecoration: "0.2em underline solid #F80469" }}
+            >
+              {resultAll + " t"}
             </div>
+            <div className={css.text} style={{ fontStyle: "italic" }}>
+              Differenz zur Klimaneutralität
+            </div>
+            <div className={css.text2} style={{ fontStyle: "italic" }}>
+              {result5 + " t"}
+            </div>
+            <div className={css.text}>Deutscher Durchschnitt</div>
+            <div className={css.text2}>{"10,41 t"}</div>
           </div>
         ) : (
           ""
@@ -270,17 +342,20 @@ export default function Calculator() {
             Zurück
           </div>
         )}
-        {pageNumber != 4 && (
-          <div
-            className={css.navButton}
-            onClick={() => {
-              setPageNumber(pageNumber != 4 ? pageNumber + 1 : pageNumber);
-              setResult(calculateResult());
-            }}
-          >
-            {pageNumber >= 3 ? "Ausrechnen" : "Weiter"}
-          </div>
-        )}
+        {
+          //Aufrufen der Rechnermethode zum Aktualisieren der Zusatndsvariablen
+          pageNumber != 4 && (
+            <div
+              className={css.navButton}
+              onClick={() => {
+                setPageNumber(pageNumber != 4 ? pageNumber + 1 : pageNumber);
+                calculateResultAll();
+              }}
+            >
+              {pageNumber >= 3 ? "Ausrechnen" : "Weiter"}
+            </div>
+          )
+        }
       </div>
     </div>
   );
