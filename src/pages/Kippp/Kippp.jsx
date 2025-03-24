f390cf04da7606c2ce171cf4e4df17568b7e8863
Selim Eser <2211482@stud.hs-mannheim.de>
2025-03-24 14:50:28 +0100

    Nadelwald stage added to Kippp

--- src/pages/Kippp/Kippp.jsx
+++ src/pages/Kippp/Kippp.jsx
@@ -3,6 +3,7 @@ import Map from "../../assets/mapwhite.svg";
 import { useState } from "react";
 import Grönland from "../../assets/schmelzen.mp4";
 import Korallenriff from "../../assets/korallenriff.mp4";
+import Nadelwald from "../../assets/nadelwald.mp4";
 import Amazonas from "../../assets/amazonas.mp4";
 import Ostantarktis from "../../assets/schmelzen.mp4";
 import Wolke from "../../assets/wolke.svg";
@@ -19,6 +20,7 @@ export default function Kippp() {
   const [mapState, setMapState] = useState(1.5);
   const [infoVisible, setInfoVisible] = useState(false);
   const [showVideo, setShowVideo] = useState(false);
+  const [infoState, setInfoState] = useState(1);
 
   return (
     //Karte verändert sich basierend auf dem aktuellen Kartenzustand,
@@ -53,11 +55,13 @@ export default function Kippp() {
             >
               <video
                 src={
-                  mapState == 1.5
+                  infoState == 1
                     ? Grönland
-                    : mapState == 2
+                    : infoState == 2
                     ? Korallenriff
-                    : mapState == 3
+                    : infoState == 3
+                    ? Nadelwald
+                    : mapState == 4
                     ? Amazonas
                     : Ostantarktis
                 }
@@ -71,24 +75,28 @@ export default function Kippp() {
 
           <div className={css.text}>
             <div className={css.heading}>
-              {mapState == 1.5
+              {infoState == 1
                 ? "Grönländisches Eisschild schmilzt"
-                : mapState == 2
+                : infoState == 2
                 ? "Korallenriffe sterben"
-                : mapState == 3
+                : infoState == 3
+                ? "Nadelwald ..."
+                : infoState == 4
                 ? "Amazonas stirbt"
-                : mapState == 5
+                : infoState == 5
                 ? "Ostantarktis schmilzt"
                 : ""}
             </div>
             <div className={css.description}>
-              {mapState == 1.5
+              {infoState == 1
                 ? "Forscher gehen davon aus, dass das grönländische Eisschild immer weiter abschmilzt, wenn die globale Erwärmung in den nächsten 10 bis 20 Jahren nicht deutlich reduziert wird. Bei einer Erwärmung um 1,5 bis 2,5 Grad ist der KIPPPunkt erreicht. Dann schrumpft der Lebensraum der dort lebenden Robben und Schalentiere dramatisch."
-                : mapState == 2
+                : infoState == 2
                 ? "Bei einer Erwärmung um mehr als 2 Grad könnten bereits 99 Prozent aller Korallen verschwunden sein. Korallen stoßen in zu warmem Wasser die Algen ab, die ihnen als Nahrung dienen, und werden weiß. Mehr CO₂ im Wasser führt auch zu einer Versauerung des Wassers, wodurch die Korallen schlechter Kalkskelette aufbauen können und dadurch langsamer und schwächer wachsen."
-                : mapState == 3
+                : infoState == 3
+                ? "Nadelwald ..."
+                : infoState == 4
                 ? "Bei einer Erderwärmung von 3 °C droht der Amazonas-Regenwald großflächig zu kollabieren. Weniger Niederschlag und häufigere Dürren könnten dazu führen, dass sich der Wald in eine trockene Savanne oder sogar eine wüstenähnliche Landschaft verwandelt. Dies würde nicht nur die Biodiversität drastisch reduzieren, sondern auch enorme Mengen an CO₂ freisetzen, was den Klimawandel weiter beschleunigt. Zudem würden regionale Wetterveränderungen, wie der Zusammenbruch des südamerikanischen Wasserkreislaufs, die Landwirtschaft und Wasserversorgung massiv gefährden."
-                : mapState == 5
+                : infoState == 5
                 ? "Bei anhaltender Erderwärmung könnten große Teile des Eisschildes instabil werden, insbesondere der Totten- und Denman-Gletscher, die riesige Mengen an Eis enthalten. Schmelzt dieses Eis, könnte der Meeresspiegel langfristig um mehrere Meter steigen. Während die Westantarktis bereits stark betroffen ist, zeigen neue Studien, dass auch die Ostantarktis Kipppunkte erreichen könnte – mit globalen Folgen für Küstenregionen und Wetterphänomene."
                 : ""}
             </div>
@@ -112,9 +120,9 @@ export default function Kippp() {
           id={css.lvl1}
           style={{ opacity: mapState >= 1.5 ? "100%" : "0%" }}
           onClick={() => {
+            setInfoState(1);
             setInfoVisible(true);
             setShowVideo(true);
-            setMapState(1.5);
           }}
         />
 
@@ -124,9 +132,9 @@ export default function Kippp() {
           id={css.lvl2}
           style={{ opacity: mapState >= 2 ? "100%" : "0%" }}
           onClick={() => {
+            setInfoState(2);
             setInfoVisible(true);
             setShowVideo(true);
-            setMapState(2);
           }}
         />
 
@@ -134,11 +142,11 @@ export default function Kippp() {
           src={Auge}
           className={css.openSequence}
           id={css.lvl3}
-          style={{ opacity: mapState >= 3 ? "100%" : "0%" }}
+          style={{ opacity: mapState >= 2 ? "100%" : "0%" }}
           onClick={() => {
+            setInfoState(3);
             setInfoVisible(true);
             setShowVideo(true);
-            setMapState(3);
           }}
         />
 
@@ -146,11 +154,23 @@ export default function Kippp() {
           src={Auge}
           className={css.openSequence}
           id={css.lvl4}
-          style={{ opacity: mapState >= 5 ? "100%" : "0%" }}
+          style={{ opacity: mapState >= 3 ? "100%" : "0%" }}
           onClick={() => {
+            setInfoState(4);
             setInfoVisible(true);
             setShowVideo(true);
-            setMapState(5);
+          }}
+        />
+
+        <img
+          src={Auge}
+          className={css.openSequence}
+          id={css.lvl5}
+          style={{ opacity: mapState >= 5 ? "100%" : "0%" }}
+          onClick={() => {
+            setInfoState(5);
+            setInfoVisible(true);
+            setShowVideo(true);
           }}
         />
 
